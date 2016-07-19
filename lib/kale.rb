071e387883323faf4c0cc52c24c531c7115597ae
nehemiahnewell <nehemiah.newell@gmail.com>
2016-07-19 08:00:43 +0000

    create message responding with error message

--- lib/kale.rb
+++ lib/kale.rb
@@ -36,5 +36,12 @@ module Kale
 
       JSON.parse response.body
     end
+    
+    def create_message(user, recipient, subj, message)
+      return self.class.post('https://www.bloc.io/api/v1/messages',
+                                 query: { user_id: user, recipient_id: recipient, subject: subj, stripped: String(message)},
+                                 headers: { 'authorization' => @auth_token })
+      
+    end
   end
 end
