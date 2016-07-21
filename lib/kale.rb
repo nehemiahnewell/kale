a2ff18ba84bc49f3b348ae4cb6b93196eedad85a
nehemiahnewell <nehemiah.newell@gmail.com>
2016-07-21 21:16:55 +0000

    Working create submission - this will create a submission, but it won't update one, so if you've already submitted to a checkpoint it won't work. It also gets a 200 back in that case, so you won't see that it didn't update. As this checkpoint asks only for a create function, this isn't actually a bug, but it is annoying.

--- lib/kale.rb
+++ lib/kale.rb
@@ -43,21 +43,13 @@ module Kale
                                  headers: { content_type: 'application/json', authorization: @auth_token })
       
     end
-    
+  
     def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
       enrollment_id = self.get_me['current_enrollment']['id']
       
-      return self.class.post('https://www.bloc.io/api/v1/messages',
-                                 query: { enrollment_id: enrollment_id, checkpoint_id: checkpoint_id, assignment_branch: String(assignment_branch), assignment_commit_link: String(assignment_commit_link), comment: String(comment) },
-                                 headers: { 'authorization' => @auth_token })
-    end
-    
-    def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
-      enrollment_id = self.get_me['current_enrollment']['id']
-      
-      return self.class.post('https://www.bloc.io/api/v1/messages',
-                                 query: { enrollment_id: enrollment_id, checkpoint_id: checkpoint_id, assignment_branch: String(assignment_branch), assignment_commit_link: String(assignment_commit_link), comment: String(comment) },
-                                 headers: { 'authorization' => @auth_token })
+      return self.class.post('https://www.bloc.io/api/v1/checkpoint_submissions',
+                                 query: { "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "checkpoint_id": checkpoint_id, "comment": comment, "enrollment_id": enrollment_id },
+                                 headers: { content_type: 'application/json', authorization: @auth_token })
     end
   end
 end
